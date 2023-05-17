aa9d195b523a406366bb49f550ffd984e0c0fbcf
Santu Sarkar <info.dip.sarkar@gmail.com>
2023-05-17 16:36:41 +0530

    final commit

--- scrape.sql
+++ scrape.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2023 at 09:26 AM
+-- Generation Time: May 17, 2023 at 01:06 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 7.4.30
 
@@ -75,7 +75,80 @@ INSERT INTO `link_data` (`id`, `link_id`, `title`, `votes`, `views`, `answer`, `
 (27, 2, 'ASP.NET Core 6 Web API : Entity Framework add record causes records to be deleted', '0', '20', '0', 0, '2023-05-17 04:10:11', '', '', ''),
 (28, 2, 'Azure DevOps - Getting &#39;Environment Variables&#39; during pipeline run', '0', '11', '0', 0, '2023-05-17 04:10:11', '', '', ''),
 (29, 2, 'Character Dash Function Is Not Working - Unity', '0', '12', '0', 0, '2023-05-17 04:10:11', '', '', ''),
-(30, 2, 'How can I get Auto Mapper to work with a custom list that inherits everything inside the List Class', '0', '17', '0', 0, '2023-05-17 04:10:11', '', '', '');
+(30, 2, 'How can I get Auto Mapper to work with a custom list that inherits everything inside the List Class', '0', '17', '0', 0, '2023-05-17 04:10:11', '', '', ''),
+(31, 3, 'Issue with passing JavaScript variable to PHP function and handling returned value [duplicate]', '0', '8', '0', 0, '2023-05-17 08:38:33', '', '', ''),
+(32, 3, 'How write to a specific line of a .txt file with php [duplicate]', '-3', '11', '0', 0, '2023-05-17 08:38:33', '', '', ''),
+(33, 3, 'ApiPlatform - Custom Action, returns an array of DTO&#39;s in attributes of the resource', '0', '8', '0', 0, '2023-05-17 08:38:33', '', '', ''),
+(34, 3, 'sort csv field closest day of week and time in textfile php', '0', '9', '0', 0, '2023-05-17 08:38:33', '', '', ''),
+(35, 3, 'How to convert ajax data into array of objects', '0', '17', '0', 0, '2023-05-17 08:38:33', '', '', ''),
+(36, 3, 'Problem with fastest delivery from vendor', '-1', '9', '0', 0, '2023-05-17 08:38:33', '', '', ''),
+(37, 3, 'Is there a method in JQuery similar to &quot;is_unique&quot; in Codeigniter-4', '0', '13', '0', 0, '2023-05-17 08:38:33', '', '', ''),
+(38, 3, 'Can any one check the php code it not working will we uploading resume [closed]', '-4', '15', '0', 0, '2023-05-17 08:38:33', '', '', ''),
+(39, 3, 'Get Sum of Different Social Insights', '-1', '26', '0', 0, '2023-05-17 08:38:33', '', '', ''),
+(40, 3, 'Call php file via axios has been blocked by CORS policy [duplicate]', '-1', '9', '0', 0, '2023-05-17 08:38:33', '', '', ''),
+(41, 3, 'Laravel installation and php.ini. issues', '-1', '14', '0', 0, '2023-05-17 08:38:33', '', '', ''),
+(42, 3, 'What does a comprehensive solution for error and exception handling look like? [closed]', '0', '21', '0', 0, '2023-05-17 08:38:33', '', '', ''),
+(43, 3, 'I using PUT method but keep showing not allowed', '-1', '29', '1', 0, '2023-05-17 08:38:33', '', '', ''),
+(44, 3, '@suppressed PHP function not working in live server', '-1', '20', '1', 0, '2023-05-17 08:38:33', '', '', ''),
+(45, 3, 'password and username in php_my_admin', '-3', '22', '0', 0, '2023-05-17 08:38:33', '', '', ''),
+(46, 4, 'Remove the password of pdf file without having label of library in Vb.net using Ironpdf or any open source Library', '0', '8', '0', 0, '2023-05-17 08:39:48', '', '', ''),
+(47, 4, 'Implicit vs explicit conversions in VB.NET', '0', '12', '0', 0, '2023-05-17 08:39:48', '', '', ''),
+(48, 4, 'Deserialize Json.net in VB.net for an array of an array', '1', '19', '1', 0, '2023-05-17 08:39:48', '', '', ''),
+(49, 4, 'Emailing Individual Users based off of department', '0', '38', '0', 0, '2023-05-17 08:39:48', '', '', ''),
+(50, 4, 'VB.NET column copy and paste in datagridview', '0', '27', '0', 0, '2023-05-17 08:39:48', '', '', ''),
+(51, 4, 'Get file path from .xlsx file in clipboard', '0', '19', '0', 0, '2023-05-17 08:39:48', '', '', ''),
+(52, 4, 'Repeat Headers in each page while printing a huge data in ASP.net', '-2', '17', '0', 0, '2023-05-17 08:39:48', '', '', ''),
+(53, 4, 'text box datas are not in symmetry', '0', '28', '0', 0, '2023-05-17 08:39:48', '', '', ''),
+(54, 4, 'Binding a dropdown list for 100+ rows speed issue', '0', '24', '0', 0, '2023-05-17 08:39:48', '', '', ''),
+(55, 4, 'Error: BC30057	Too many arguments to &#39;Public Shared Overloads Function Copy(str As String) As String&#39;', '0', '33', '1', 0, '2023-05-17 08:39:48', '', '', ''),
+(56, 4, 'Visual Studio doesn&#39;t recognize UnitTesting namespace', '0', '24', '1', 0, '2023-05-17 08:39:48', '', '', ''),
+(57, 4, 'Why is my boolean is not setting to true when it should be?', '-1', '47', '0', 0, '2023-05-17 08:39:48', '', '', ''),
+(58, 4, 'vb.net winforms, what is the e.handled = true equivalent for anything to do with column headers in a dgv?', '0', '18', '1', 0, '2023-05-17 08:39:48', '', '', ''),
+(59, 4, 'Compare a value against values in a list C#/VB.NET', '0', '72', '3', 0, '2023-05-17 08:39:48', '', '', ''),
+(60, 4, 'VB.net DataGridView rounding decimal to integer', '0', '25', '1', 0, '2023-05-17 08:39:48', '', '', ''),
+(61, 5, 'I can&#39;t install pip mysql-connector', '0', '4', '0', 0, '2023-05-17 08:40:27', '', '', ''),
+(62, 5, 'MySQL How to SELECT WHERE NOT EXISTS from two table along with the name of description', '0', '13', '0', 0, '2023-05-17 08:40:27', '', '', ''),
+(63, 5, 'Require some Large SaaS Advice for an existing platform', '0', '9', '0', 0, '2023-05-17 08:40:27', '', '', ''),
+(64, 5, 'Variable declared on the previous line does not seem to exist', '1', '13', '0', 0, '2023-05-17 08:40:27', '', '', ''),
+(65, 5, 'Using Round Robin Method in Node Js, How Can I Assigned a Lead to the Next Professional', '0', '14', '1', 0, '2023-05-17 08:40:27', '', '', ''),
+(66, 5, 'Kubernetes: deploy a web app using tomcat and mysql', '-1', '17', '0', 0, '2023-05-17 08:40:27', '', '', ''),
+(67, 5, 'mysql like query for at least one word from searched string [duplicate]', '0', '16', '0', 0, '2023-05-17 08:40:27', '', '', ''),
+(68, 5, 'Retrieve data faster through sqlalchemy query to RDS (Aurora) Database', '0', '3', '0', 0, '2023-05-17 08:40:27', '', '', ''),
+(69, 5, 'phpmyadmin split string in column by delimiter to open columns to the right', '0', '16', '0', 0, '2023-05-17 08:40:27', '', '', ''),
+(70, 5, 'how can JSON data be populated in ListView in .NET Maui MVVM? [closed]', '0', '16', '0', 0, '2023-05-17 08:40:27', '', '', ''),
+(71, 5, 'Wampserver returning 403 Access Forbidden error when accessed over WAN/Internet [closed]', '-2', '11', '1', 0, '2023-05-17 08:40:27', '', '', ''),
+(72, 5, 'Automatically add a prefix or suffix onto all columns after a JOIN [duplicate]', '0', '26', '1', 0, '2023-05-17 08:40:27', '', '', ''),
+(73, 5, 'SQL server not showing any query results but MySQL workbench shows results [closed]', '-2', '23', '0', 0, '2023-05-17 08:40:27', '', '', ''),
+(74, 5, 'How to restore sys database in MySql? [closed]', '-4', '18', '0', 0, '2023-05-17 08:40:27', '', '', ''),
+(75, 6, 'Replacing two items with one item in a list -- python', '0', '10', '0', 0, '2023-05-17 08:41:48', '', '', ''),
+(76, 6, 'Why code changes doesn&#39;t do anything in VS Code?', '-1', '18', '0', 0, '2023-05-17 08:41:48', '', '', ''),
+(77, 6, 'Installing AWS EB CLI using pip fails due to breaking changes in urllib 2', '0', '3', '0', 0, '2023-05-17 08:41:48', '', '', ''),
+(78, 6, 'Why google analytics (GA4) does not track the gender of all my website users', '-1', '13', '0', 0, '2023-05-17 08:41:48', '', '', ''),
+(79, 6, 'Upgrade a self hosted python package from within a python session', '0', '7', '0', 0, '2023-05-17 08:41:48', '', '', ''),
+(80, 6, 'Scraping a JS heavy website on Windows - using Scrapy + (Playwright/Selenium/Puppeteer) excluding Splash', '0', '5', '0', 0, '2023-05-17 08:41:48', '', '', ''),
+(81, 6, 'Create table instance not connected to a document with python-docx?', '-1', '4', '0', 0, '2023-05-17 08:41:48', '', '', ''),
+(82, 6, 'Creating dynamic models in Appconfig.ready', '0', '5', '0', 0, '2023-05-17 08:41:48', '', '', ''),
+(83, 6, 'Error with pip: &#39;2020-resolver&#39; invalid choice', '0', '10', '0', 0, '2023-05-17 08:41:48', '', '', ''),
+(84, 6, 'Python Script Referencing or Calling', '-1', '11', '0', 0, '2023-05-17 08:41:48', '', '', ''),
+(85, 6, 'what happens when boolean mask of numpy array is just a single value', '0', '15', '1', 0, '2023-05-17 08:41:48', '', '', ''),
+(86, 6, 'Puhing polars dataframe to redis', '0', '7', '0', 0, '2023-05-17 08:41:48', '', '', ''),
+(87, 6, 'Issues creating a regex to extract code from Markdown', '-2', '14', '0', 0, '2023-05-17 08:41:48', '', '', ''),
+(88, 6, 'My {{ item.img.url }} doesnt work, doesnt even display url', '0', '14', '1', 0, '2023-05-17 08:41:48', '', '', ''),
+(89, 7, 'local image is not showing in reactjs', '0', '3', '0', 0, '2023-05-17 09:18:36', '', '', ''),
+(90, 7, 'Typescript unable to infer the forwardRef return type', '0', '2', '0', 0, '2023-05-17 09:18:36', '', '', ''),
+(91, 7, 'Apple pay on react web application', '0', '2', '0', 0, '2023-05-17 09:18:36', '', '', ''),
+(92, 7, 'Fetch error &quot;can&#39;t fetch&quot; when submitting a form in React', '0', '8', '0', 0, '2023-05-17 09:18:36', '', '', ''),
+(93, 7, 'Session lost, when fetch data with react from PHP API', '0', '17', '0', 0, '2023-05-17 09:18:36', '', '', ''),
+(94, 7, 'How to make fetch call again in React Server Components with new state like UseEffect with Next 13.4', '0', '8', '0', 0, '2023-05-17 09:18:36', '', '', ''),
+(95, 7, 'Connecting language change to url change in a different file', '0', '5', '0', 0, '2023-05-17 09:18:36', '', '', ''),
+(96, 7, 'Typscript keeps throwing Property &#39;children&#39; does not exist on type &#39;FC&lt;ProvidersProps&gt;&#39; even after providing type', '0', '10', '1', 0, '2023-05-17 09:18:36', '', '', ''),
+(97, 7, 'Scoped css React.js alternative', '0', '8', '0', 0, '2023-05-17 09:18:36', '', '', ''),
+(98, 7, 'MERN stack user registration form not working', '0', '14', '1', 0, '2023-05-17 09:18:36', '', '', ''),
+(99, 7, 'In next.js, swipe&#39;s auto slide function doesn&#39;t work. I&#39;m not sure what&#39;s wrong', '0', '5', '0', 0, '2023-05-17 09:18:36', '', '', ''),
+(100, 7, 'Building a React component library with Parcel for use in an HTML page', '0', '11', '0', 0, '2023-05-17 09:18:36', '', '', ''),
+(101, 7, 'CSS file is not working in my NPM package bundles by eslint', '-1', '6', '0', 0, '2023-05-17 09:18:36', '', '', ''),
+(102, 7, 'Property refresh does not exist on type NextRouter', '0', '3', '0', 0, '2023-05-17 09:18:36', '', '', ''),
+(103, 7, 'Getting an empty object when i pass my file to formData', '0', '14', '1', 0, '2023-05-17 09:18:36', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -100,7 +173,12 @@ CREATE TABLE `link_names` (
 
 INSERT INTO `link_names` (`id`, `link_name`, `link`, `total_questions`, `created_at`, `created_by`, `updated_at`, `updated_by`) VALUES
 (1, 'C', 'https://stackoverflow.com/questions/tagged/c?tab=newest&pagesize=15', 396213, '2023-05-17 04:09:02', NULL, NULL, NULL),
-(2, 'C#', 'https://stackoverflow.com/questions/tagged/c%23?tab=newest&pagesize=15', 1591425, '2023-05-17 04:10:09', NULL, NULL, NULL);
+(2, 'C#', 'https://stackoverflow.com/questions/tagged/c%23?tab=newest&pagesize=15', 1591425, '2023-05-17 04:10:09', NULL, NULL, NULL),
+(3, 'PHP', 'https://stackoverflow.com/questions/tagged/php?tab=newest&pagesize=15', 1458571, '2023-05-17 08:38:02', NULL, NULL, NULL),
+(4, 'Vb.net', 'https://stackoverflow.com/questions/tagged/vb.net?tab=newest&pagesize=15', 139367, '2023-05-17 08:39:42', NULL, NULL, NULL),
+(5, 'mysql', 'https://stackoverflow.com/questions/tagged/mysql?tab=newest&pagesize=15', 660026, '2023-05-17 08:40:16', NULL, NULL, NULL),
+(6, 'Python', 'https://stackoverflow.com/questions/tagged/python?tab=newest&pagesize=15', 2135439, '2023-05-17 08:41:38', NULL, NULL, NULL),
+(7, 'Reactjs', 'https://stackoverflow.com/questions/tagged/reactjs?tab=Newest', 455139, '2023-05-17 09:18:34', NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -126,13 +204,13 @@ ALTER TABLE `link_names`
 -- AUTO_INCREMENT for table `link_data`
 --
 ALTER TABLE `link_data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=104;
 
 --
 -- AUTO_INCREMENT for table `link_names`
 --
 ALTER TABLE `link_names`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
